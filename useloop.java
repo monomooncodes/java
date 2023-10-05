dd7c47f2ea433cc3eb474fb853d042f6e77d963f
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-10-05 17:01:01 +0000

    code

---
+++ useloop.java
@@ -0,0 +1,12 @@
+public class useloop{
+public static void main (String[] args){
+    int[] a={5,7,8,5,8,4,9,4,6,2};
+    int sum = 0 ;
+    for(int i=0; i<a.length;i++)
+    {
+sum+=a[i];
+    }
+    System.out.println(sum);
+
+}
+}
