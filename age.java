b0b8b9bbf883afd918f7b5393c9cf81ff93891b5
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-10-05 15:49:39 +0000

    codes

---
+++ age.java
@@ -0,0 +1,14 @@
+// if(){
+    
+// }
+public class age{
+    public static void main(String[] args){
+        int a = 20;
+       if(a>19){
+System.out.println("you can drink 8848");
+
+       }  else {
+        System.out.println("susie cant drink 8848");
+       }
+    }
+}
