643ae07ed1da62c21978bd9226ef95993dce67c5
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-10-05 16:07:49 +0000

    code

---
+++ greaternum.java
@@ -0,0 +1,22 @@
+public class greaternum{
+    public static void main(String[] args){
+        int a = 3;
+        int b = 2;
+        int c = 1;
+
+    
+    if(a>b|| a>c){
+
+    
+    System.out.println("a is greater number ");
+    }
+    else if(b>a||b>c)
+    {
+ System.out.println("b is greater number");
+    }
+    else {
+        System.out.println("c is greater number");
+    }
+}
+
+}
