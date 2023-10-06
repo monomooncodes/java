75a9d041a6001aa43e6be066d1257c3565515c42
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-10-06 16:34:24 +0000

    code

---
+++ areaofcircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class areaofcircle{
+ 
+ public static void main (String[] args){
+// making oject
+ Scanner s = new Scanner(System.in);
+System.out.println("enter ratious");
+double r=s.nextDouble();
+double area=(22*r*r)/7;
+System.out.println("your area of circle is" + area);
+ }
+
+}
